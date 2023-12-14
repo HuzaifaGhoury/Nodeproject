ff195b834a623943a3d86fe08b1447d267e91169
HuzaifaGhoury <huzaifaghourydev@gmail.com>
2023-12-14 04:07:20 -0800

    create Api

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:"zoya",email:"zoya@gmail.com"},
+    {name:"john",email:"john@gmail.com"},
+    {name:"dev",email:"dev@gmail.com"},
+]
+module.exports=data
--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-// const simple = require("./moduleExport")
-// console.log(simple())
-const fs = require('fs')
-const http = require("http")
-const port = process.env.PORT || 3000
+const fs = require("fs");
+const http = require("http");
+const data = require('./data')
+const port = process.env.PORT || 3100;
+const express = require('express')
 
-const server = http.createServer((req,res)=>{
-    res.setHeader('content-Type','text/html')
-    if(req.url == '/'){
-        res.statusCode = 200
-        res.end('<h1>This is home Page</h1>')
-        // const data = fs.readFileSync(index.html)
-        // res.end(data.toString())
-    }
-   else if(req.url == '/about'){
-        res.statusCode = 200
-        res.end('<h1>This is About Page</h1>')
-    }
-    else{
-        res.statusCode = 404
-        res.end('<h1>Page is not found</h1>')
-    }
+//HTTP ROUTES
 
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  if (req.url === "/") {
+    res.statusCode = 200;
+    const data = fs.readFileSync("home.html");
+    res.end(data.toString());
+  } else if (req.url === "/about") {
+    res.statusCode = 200;
+    res.end("<h1>This is About Page</h1>");
+  } else if (req.url === "/page") {
+    res.statusCode = 200;
+    const data = fs.readFileSync("index.html");
+    res.end(data.toString());
+  } else if (req.url === "/contact") {
+    res.statusCode = 200;
+    const data = fs.readFileSync("contact.html");
+    res.end(data.toString());
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>Page is not found</h1>");
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Running in port ${port}`);
+});
+
+//POSTMAN API
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify(data));
+  res.end();
+}).listen(3000);
+
+//USE EXPRESS
+
+const app = express()
+app.get('/',(req,res)=>{
+    res.send("HELLO : This is home page")
 })
-
-server.listen(port,()=>{
-    console.log(`Running in port ${port}`)
+app.get('/about',(req,res)=>{
+    res.send("HELLO : This is about page")
 })
+app.listen(3110)
